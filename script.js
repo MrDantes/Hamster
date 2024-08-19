cb2444ef28322e1ce81cc8c8ffff5d735af3dae6
MrDantes <89605609+MrDantes@users.noreply.github.com>
2024-08-19 03:37:09 +0300

    v1.0.4 - add map "2024-08-19"

--- script.js
+++ script.js
@@ -508,7 +508,6 @@ const maps = {
         key: {id: 12, x: 2, y: 2, width: 2, height: 1, direction: 'horizontal'},
         exitPosition: {x: 6, y: 2}
     },
-  },
     '2024-08-19': {
         blocks: [
             {id: 1, x: 0, y: 0, width: 1, height: 2, direction: 'vertical', color: 'red'},
